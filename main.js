7812b979fa932e5fbaeddd6c842a7f35e4706c6f
Gabriele Mercurio <gabrielemercurio.web@gmail.com>
2020-05-22 17:36:29 +0200

    Stilizzazione Input + bottone (Bootstrap 4.5)

--- main.js
+++ main.js
@@ -1,14 +1,7 @@
-// Con una chiamata ajax, recuperare i dischi musicali restituiti dall'api:
-// https://flynn.boolean.careers/exercises/api/array/music
-// Ciclare quindi i dischi e ottenuti e per ognuno di essi disegnare in pagina una card utilizzando handlebars.
 
-// BONUS: creare una select con i generi dei dischi musicali (pop, rock, metal, jazz), tramite la quale si possono filtrare i dischi visualizzati (ad esempio: se nella tendina si seleziona il genere "metal", nella pagina saranno mostrati solo i dischi con il genere "metal").
 
 
 $(document).ready(function() {
-
-    var source = $("#disc-template").html();
-    var template = Handlebars.compile(source);
 
     $.ajax ({
         'url': 'https://flynn.boolean.careers/exercises/api/array/music',
@@ -43,19 +36,6 @@ $(document).ready(function() {
         }
     });
 
-});
-
-
-$('#genere').change(function () {
-
-    var option = $(this).val();
-
-    $('.cd').addClass('invisible');
-    $('.' + option).removeClass('invisible');
-
-    if (option == 'Tutti') {
-        $('.cd').removeClass('invisible');
-    }
 });
 
 
