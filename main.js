19423f6de9c207d312cea9a15308ee1c594c7b91
Gabriele Mercurio <gabrielemercurio.web@gmail.com>
2020-05-25 18:53:22 +0200

    Sostituzione del voto con n. stelle

--- main.js
+++ main.js
@@ -1,67 +1,110 @@
 
-
-
 $(document).ready(function() {
+
+    var source = $("#movie-template").html();
+    var template = Handlebars.compile(source);
 
     // intercetto il click sul bottone 'Cerca'
     $('.search').click(function () {
-
-        $('.dvd-container li').remove();
         cerca_film();
-
     });
 
     // intercetto il tasto Enter sulla tastiera
     $('.input-cerca').keypress(function (event) {
         if (event.which == 13) {
 
-            $('.dvd-container li').remove();
+            $('.movie-container ul').remove();
             cerca_film();
         }
     });
 
+
     function cerca_film () {
 
         //leggo la query inserita dall'utente all'interno dell'input
-        var input_utente = $('.input-cerca').val();
-        console.log(input_utente);
+        var input_utente = $('.input-cerca').val().trim();
 
-        // effettuo una chiamata ajax per prelevare alcuni dati dall'api
-        $.ajax ({
-            'url': 'https://api.themoviedb.org/3/search/movie',
-            'method': 'GET',
-            'success': function(data) {
+        if (input_utente.length > 1) {
 
-                var risposta_api = data.results;
+            $('.dvd-container li').remove();
 
-                for (var i = 0; i < risposta_api.length; i++) {
+            // effettuo una chiamata ajax per prelevare alcuni dati dall'api
+            $.ajax ({
+                'url': 'https://api.themoviedb.org/3/search/movie',
+                'method': 'GET',
+                'data': {
+                    'api_key': 'd9712716ad212136c0d42333f3638448',
+                    'query': input_utente, // <-- .val() dell'input inserito dall'utente
+                    'language': 'it'
+                },
 
-                    var titolo = risposta_api[i].title;
-                    var titolo_originale = risposta_api[i].original_title;
-                    var lingua = risposta_api[i].original_language;
-                    var voto = risposta_api[i].vote_average;
+                'success': function(data) {
 
-                    var movie = $('.dvd-container ul').append('<li>' + titolo + ' - ' + lingua + ' - ' + titolo_originale + ' - ' + voto + '</li>');
+                    // Titolo dei risultati di ricerca nel DOM'
+                    $('.ricerca-utente').text(input_utente);
+                    $('#risultato-ricerca').addClass('visible');
+
+                    var risposta_api = data.results;
+
+                    for (var i = 0; i < risposta_api.length; i++) {
+
+                        var movie = {
+                            'titolo': risposta_api[i].title,
+                            'titolo_originale': risposta_api[i].original_title,
+                            'lingua': risposta_api[i].original_language,
+                            'voto': genenera_stella(risposta_api[i].vote_average)
+                        };
+
+                        var html_card = template(movie);
+                        $('.movie-container').append(html_card);
+                    }
+
+                },
+
+                'error': function () {
+                    console.log('Errore!');
                 }
 
-            },
-            'error': function () {
-                console.log('Errore!');
-            },
+            });
 
-            'data': {
-                'api_key': 'd9712716ad212136c0d42333f3638448',
-                'query': input_utente, // <-- .val() dell'input inserito dall'utente
-                'language': 'it'
-            },
-        });
+            // svuoto il campo input dopo ogni ricerca
+            $('.input-cerca').val('');
 
-        // svuoto il campo input dopo ogni ricerca
-        $('.input-cerca').val('');
+        } else {
+            $('.input-cerca').val('');
+            alert('Attenzione! Devi digitare almeno 2 caratteri.');
+            }
 
     } // <-- Fine funzione cerca_film
 
 });
+
+
+function genenera_stella(voti) {
+    // trasformo la votazione da 1/10 a 1/5
+    var voto_medio = Math.ceil(voti / 2);
+    var stella_piena = '';
+    var stella_vuota = '';
+    // inserisco tante stella_piena quanto è il voto_medio
+    for (var i = 0; i < voto_medio; i++) {
+        stella_piena += '<i class="star material-icons">star</i>';
+    }
+    // Inserisco tante stella_vuota quanto è la sottrazione tra 5 e il voto_medio
+    for (var i = 0; i < 5-voto_medio; i++) {
+        stella_vuota += '<span class="star_border material-icons">star_border</span>';
+    }
+
+    return stella_piena + stella_vuota;
+}
+
+
+
+
+
+
+
+
+
 
 
 
